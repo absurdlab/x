1d87cb70d9e867659b346330a6ba02f2c4355d34
Weinan Qiu <davidiamyou@gmail.com>
2021-01-16 11:50:01 +0800

    add IsURL to stringz

--- v2/stringz/format.go
+++ v2/stringz/format.go
@@ -1,8 +1,17 @@
 package stringz
 
-import "strings"
+import (
+	"net/url"
+	"strings"
+)
 
 // SpaceJoin joins values with space
 func SpaceJoin(values ...string) string {
 	return strings.Join(values, " ")
+}
+
+// IsURL checks if the value is a valid url.
+func IsURL(v string) bool {
+	_, err := url.ParseRequestURI(v)
+	return err == nil
 }
